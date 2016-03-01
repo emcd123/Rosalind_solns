ee2ff57cedf38376b341482e45fd697c1018b607
emcd123 <mcdonnell.owen1@gmail.com>
2016-03-01 13:49:07 +0000

    Update geneOrdering.py

--- geneOrdering.py
+++ geneOrdering.py
@@ -4,9 +4,20 @@ def geneOrder(n):
 	seq = [] 
 	for i in range(1,n+1):
 		seq.append(i)
-		seq.append(i*-1)
-	print(seq)
-	length = len(seq)
+	seqList = map(list,list(itertools.permutations(seq,n)))
+	resultSeqList = []
+	for i in seqList:
+		signalFlag=1
+		for j in i:
+			if ((j in i) & ((j*-1) in i)):
+                    signalFlag = 1
+                    break
+            if signalFlag == 0:
+                    resultSeqList.append(i)
+
+	print(len(resultSeqList))
+	for i in resultSeqList:
+		print(' '.join(map(str,i)))
 
 
 geneOrder(n)
